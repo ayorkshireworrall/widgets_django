80ce017bdc662d1bf04fc700b7791202bdc6cca8
aworrall <alex.worrall@keytree.co.uk>
2020-08-19 17:34:01 +0100

    Add a model

--- widget_project/widget/models.py
+++ widget_project/widget/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Widget(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    description = models.CharField(blank=False, max_length=1000, default='')
+    
+
+    class Meta:
+        verbose_name = "Widget"
+        verbose_name_plural = "Widgets"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Widget_detail", kwargs={"pk": self.pk})
