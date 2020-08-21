e6dad8ac4782334b890413c15ef3045ffc36366b
aworrall <alex.worrall@keytree.co.uk>
2020-08-21 11:41:28 +0100

    Defining custom requests

--- widget_project/widget/urls.py
+++ widget_project/widget/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/widgets/', views.WidgetListCreate.as_view()),
+    path('api/v1/widgets/', views.WidgetListCreate.as_view()),
+    path('api/v2/widgets/', views.widget_list),
+    path('api/v2/widgets/<int:pk>', views.widget_detail),
 ]
--- widget_project/widget/views.py
+++ widget_project/widget/views.py
@@ -1,7 +1,50 @@
 from .models import Widget
 from .serializers import WidgetSerializer
 from rest_framework import generics
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 class WidgetListCreate(generics.ListCreateAPIView):
     queryset = Widget.objects.all()
     serializer_class = WidgetSerializer
+
+#TODO remove exemption when authentication in place
+@csrf_exempt
+def widget_list(request):
+    if request.method == 'GET':
+        widgets = Widget.objects.all()
+        serializer = WidgetSerializer(widgets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = WidgetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+#TODO remove exemption when authentication in place
+@csrf_exempt
+def widget_detail(request, pk):
+    try:
+        widget = Widget.objects.get(pk=pk)
+    except Widget.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = WidgetSerializer(widget)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = WidgetSerializer(widget, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+     
+    elif request.method == 'DELETE':
+        widget.delete()
+        return HttpResponse(status=204)
